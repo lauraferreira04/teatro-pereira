c301a49b03aa98b7229b4f9602827c02b2b01f85
gabrielhackenhaar <gabrielhackenhaar9@gmail.com>
2023-09-09 17:56:42 -0300

    MOBILE [FEATURE]: criação da base das pastas e configuração inicial do projeto, criação da tela login e cadastro.

---
+++ Mobile/app/src/main/java/com/example/teatropereira_mobile/view/CadastroFragment.java
@@ -0,0 +1,38 @@
+package com.example.teatropereira_mobile.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.teatropereira_mobile.R;
+import com.example.teatropereira_mobile.databinding.FragmentCadastroBinding;
+
+public class CadastroFragment extends Fragment {
+
+    FragmentCadastroBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_cadastro, container, false);
+        binding = FragmentCadastroBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
