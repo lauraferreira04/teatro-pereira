0297c92d4806b04d5f915c56aec9a5e82de32e79
gabrielhackenhaar <gabrielhackenhaar9@gmail.com>
2023-09-13 11:54:39 -0300

    MOBILE [FEATURES]: novas telas criadas

---
+++ Mobile/app/src/main/java/com/example/teatropereira_mobile/view/EventosFragment.java
@@ -0,0 +1,39 @@
+package com.example.teatropereira_mobile.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.teatropereira_mobile.R;
+import com.example.teatropereira_mobile.databinding.FragmentEventosBinding;
+
+public class EventosFragment extends Fragment {
+
+    FragmentEventosBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_eventos, container, false);
+        binding = FragmentEventosBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
