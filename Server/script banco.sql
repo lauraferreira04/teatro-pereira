3afbcbb9ae3a0ac5ed1447d05734c2e4a489a3b1
lauraferreira04 <lauraferreiraa015@gmail.com>
2023-09-13 11:17:39 -0300

    SERVER [FEATURE]: Adicionado NOT NULL nos atributos necessários.

--- Server/script banco.sql
+++ Server/script banco.sql
@@ -5,33 +5,33 @@ use teatropereira;
 
 CREATE TABLE usuario (
   idusuario int NOT NULL AUTO_INCREMENT,
-  nomeusuario varchar(50),
-  login varchar(40),
-  senha varchar(40),
-  cpf varchar(11),
-  email varchar(60),
-  telefone varchar(20),
-  tipo tinyint COMMENT '0 -> admin | 1 -> comum',
+  nomeusuario varchar(50) NOT NULL,
+  login varchar(40) NOT NULL,
+  senha varchar(40) NOT NULL,
+  cpf varchar(14) NOT NULL,
+  email varchar(60) NOT NULL,
+  telefone varchar(20) NOT NULL,
+  tipo tinyint NOT NULL COMMENT '0 -> admin | 1 -> comum',
   PRIMARY KEY (idusuario)
 );
 
 CREATE TABLE evento (
   idevento int NOT NULL AUTO_INCREMENT,
-  nomeartista varchar(50),
-  nomeevento varchar(50),
-  datahora datetime,
-  valor decimal(10,2),
-  qtdcadeiras int,
-  banner longblob,
+  nomeevento varchar(50) NOT NULL,
+  nomeartista varchar(50) NOT NULL,
+  datahora datetime NOT NULL,
+  valor decimal(10,2) NOT NULL,
+  qtdcadeiras int NOT NULL,
+  banner longblob NOT NULL,
   PRIMARY KEY (idevento)
 );
 
 CREATE TABLE reserva (
   idreserva int not null auto_increment,
   idevento int NOT NULL ,
-  idusuario int not null,
-  qtdingressos int,
-  valortotal decimal(10,2),
+  idusuario int NOT null,
+  qtdingressos int NOT NULL,
+  valortotal decimal(10,2) NOT NULL,
   PRIMARY KEY (idreserva),
   FOREIGN KEY (idevento) REFERENCES evento (idevento),
   FOREIGN KEY (idusuario) REFERENCES usuario (idusuario)
