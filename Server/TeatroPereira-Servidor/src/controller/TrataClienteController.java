d02e057b3261cac6d8c0776bdf92e4a447529782
lauraferreira04 <lauraferreiraa015@gmail.com>
2023-09-27 11:57:19 -0300

    SERVER [FEATURE]: Iniciado o TrataClienteController.

--- Server/TeatroPereira-Servidor/src/controller/TrataClienteController.java
+++ Server/TeatroPereira-Servidor/src/controller/TrataClienteController.java
@@ -4,10 +4,32 @@
  */
 package controller;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *  trabalho interdisciplinar
  * @author laura ferreira & gabriel hackenhaar
  */
-public class TrataClienteController {
+public class TrataClienteController extends Thread{
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private int idUnico;
+    
+    public TrataClienteController(Socket socket, int idUnico) {
+        this.socket = socket;
+        this.idUnico = idUnico;
+        try {
+            this.in = new ObjectInputStream(this.socket.getInputStream());
+            this.out = new ObjectOutputStream(this.socket.getOutputStream());
+            this.start();
+        } catch(IOException ioe) {
+            System.out.println("Erro: " + ioe.getMessage());
+        }
+    }
+    
     
 }
